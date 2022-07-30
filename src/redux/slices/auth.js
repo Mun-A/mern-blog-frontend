2af5724c6de14a5c22f1344634dc8b7c74ca69e9
mun-a <karnevall.13.13@gmail.com>
2022-07-30 11:30:54 +0500

    add authorization and logout

---
+++ src/redux/slices/auth.js
@@ -0,0 +1,45 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+import axios from "../../axios";
+
+export const fetchAuth = createAsyncThunk("auth/fetchAuth", async (params) => {
+  const { data } = await axios.post("/auth/login", params);
+
+  return data;
+});
+
+const initialState = {
+  data: null,
+  status: "loading",
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    logout: (state) => {
+      state.data = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAuth.pending, (state) => {
+        state.status = "loading";
+        state.data = null;
+      })
+      .addCase(fetchAuth.fulfilled, (state, action) => {
+        state.status = "success";
+        state.data = action.payload;
+      })
+      .addCase(fetchAuth.rejected, (state) => {
+        state.status = "error";
+        state.data = null;
+      });
+  },
+});
+
+export const selectIsAuth = (state) => Boolean(state.auth.data);
+
+export const { logout } = authSlice.actions;
+
+export const authReducer = authSlice.reducer;
